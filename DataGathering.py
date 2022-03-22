d8b38a4fe2931f5cfdf94557fb9a212c0b82e9ab
A.Gokula Sai Bhargava Goud <94063285+gokulasai9@users.noreply.github.com>
2022-03-22 23:13:16 +0530

    Add files via upload

---
+++ DataGathering.py
@@ -0,0 +1,50 @@
+from cProfile import label
+from multiprocessing import dummy
+from operator import index
+import paho.mqtt.client as mqtt
+import pandas as pd
+
+sub_client = mqtt.Client()
+sub_client.connect('broker.hivemq.com',1883)
+print('Broker Connected')
+
+sub_client.subscribe('gpcet/ai')
+
+data = []
+i = 0
+
+
+
+def notification(sub_client,userdata,msg):
+    global data,i
+    k = msg.payload.decode('utf-8')
+    k = k.split(':')
+    h = int(k[1].split(',')[0])
+    t = int(k[-1][:-1])
+    #print(k,type(k),h,t)
+    label = 0
+    if ( h > 40 and h <= 60):
+        label = 1
+    elif ( h > 60 and h <= 80):
+        label = 2
+    elif ( h > 80 and h <= 100):
+        label = 3
+    else:
+        label = 0
+    #print(h,t,label)
+    dummy = []
+    dummy.append(h)
+    dummy.append(t)
+    dummy.append(label)
+    data.append(dummy)
+    print(data)
+    i += 1
+    if ( i == 300 ):
+
+        df = pd.DataFrame(data)
+        df.to_csv('dataset.csv')
+        i = 0
+    
+
+sub_client.on_message = notification
+sub_client.loop_forever()
